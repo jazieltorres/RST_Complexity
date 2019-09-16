2a7f6605a33674c57fd5f6d18cad07c924d0f47b
Luis <luis.quinones21@upr.edu>
2019-09-16 13:59:20 -0400

    files for CLion support

---
+++ cmake-build-debug/CMakeFiles/RST.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RST.dir/main.cpp.o"
+  "RST.pdb"
+  "RST"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RST.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
