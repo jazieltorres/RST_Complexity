97867ce39e08e9c887e05bc30858f4f5ce678464
jazieltorres <jaziel.torres@upr.edu>
2019-09-19 19:30:01 -0400

    Added MultiDimArray class and changed main to include only MultiDimArray

--- main.cpp
+++ main.cpp
@@ -1,17 +1,9 @@
 #include <iostream>
-#include <vector>
-#include "MExponent.h"
+#include "MultiDimArray.h"
 
 using namespace std;
 
 int main() {
-    vector<long> v1 = {3,3,0} ;
-    vector<long> v2 = {4,2,0} ;
-    MExponent m1(v1) ;
-    MExponent m2(v2) ;
-    MExponent m3 = m1+m2 ;
-    if (m1.grlex_less(m2)) cout << m1 ;
-    else cout << m2 ;
-    // cout << m1+m2 << endl ; // no se puede
+    cout << "Compiles!" << endl;
     return 0;
 }
