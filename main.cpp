237aa0cee01c87bcb306bf2a676571234e4b7fc8
jazieltorres <jaziel.torres@upr.edu>
2019-09-21 18:19:00 -0400

    Nothing important was here. Now is empty, only return 0.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
-#include "MultiDimArray.h"
+#include "MultiDimArray.hpp"
 
 using namespace std;
 
+
 int main() {
-    cout << "Compiles!" << endl;
+    blitz::TinyVector<long, 5> A(3);
+    blitz::TinyVector<long, 5> B(4);
+    cout << A << endl;
     return 0;
 }
+
+
+// g++ -std=c++11 -pthread -march=native main.cpp  -lntl -lblitz -lgmp -lm
