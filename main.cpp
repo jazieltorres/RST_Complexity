38bc884f5220fad0a0121c0098fa777ca3f9da57
jazieltorres <jaziel.torres@upr.edu>
2019-10-02 19:12:46 -0400

    Added test 3

--- main.cpp
+++ main.cpp
@@ -4,37 +4,50 @@
 using namespace blitz;
 
 int main() {
-//    NTL::ZZ p = (NTL::ZZ) 11;
-//    NTL::ZZ_p::init(p);
-//
-//    typedef NTL::ZZ_p F;
-//    const long m = 2;
-//
-//    blitz::Array<F,m> A(2,2);
-//    A = (F)3, (F)10,
-//        (F)1, (F)8;
-
-    NTL::ZZ p = (NTL::ZZ) 2;
+// TEST #1
+    NTL::ZZ p = (NTL::ZZ) 11;
     NTL::ZZ_p::init(p);
+
     typedef NTL::ZZ_p F;
     const long m = 2;
-//    blitz::TinyVector<unsigned long, 2> v1, v2;
-//    v1 = 0,2;
-//    v2 = 1,1;
-//    MExponent<2> e1(v1), e2(v2);
-//    cout << e1 << " < " << e2 << " : " << e1.grlex_less(e2) << endl;
-//    cout <<"SUM: " << sum(v1) << endl;
-    blitz::Array<F,m> A(6,7);
-    A = (F)0, (F)0, (F)1, (F)1, (F)0, (F)1, (F)0,
-        (F)1, (F)0, (F)0, (F)0, (F)1, (F)1, (F)0,
-        (F)0, (F)0, (F)0, (F)1, (F)1, (F)0, (F)1,
-        (F)1, (F)1, (F)0, (F)1, (F)0, (F)0, (F)0,
-        (F)0, (F)1, (F)0, (F)0, (F)0, (F)1, (F)1,
-        (F)1, (F)0, (F)1, (F)0, (F)0, (F)0, (F)1;
 
-
+    blitz::Array<F,m> A(2,2);
+    A = (F)3, (F)10,
+        (F)1, (F)8;
     MultiDimArray<F,m> array(A);
     array.RST();
+
+
+
+// TEST #2
+//    NTL::ZZ p = (NTL::ZZ) 2;
+//    NTL::ZZ_p::init(p);
+//    typedef NTL::ZZ_p F;
+//    const long m = 2;
+//    blitz::Array<F,m> A(6,7);
+//    A = (F)0, (F)0, (F)1, (F)1, (F)0, (F)1, (F)0,
+//        (F)1, (F)0, (F)0, (F)0, (F)1, (F)1, (F)0,
+//        (F)0, (F)0, (F)0, (F)1, (F)1, (F)0, (F)1,
+//        (F)1, (F)1, (F)0, (F)1, (F)0, (F)0, (F)0,
+//        (F)0, (F)1, (F)0, (F)0, (F)0, (F)1, (F)1,
+//        (F)1, (F)0, (F)1, (F)0, (F)0, (F)0, (F)1;
+//    MultiDimArray<F,m> array(A);
+//    array.RST();
+
+
+// TEST #3
+//    NTL::ZZ p = (NTL::ZZ) 11;
+//    NTL::ZZ_p::init(p);
+//    typedef NTL::ZZ_p F;
+//    const long m = 2;
+//    blitz::Array<F,m> A(5,5);
+//    A = (F)4, (F)5, (F)7, (F)0, (F)8,
+//        (F)8, (F)9, (F)0, (F)4, (F)1,
+//        (F)0, (F)1, (F)3, (F)7, (F)0,
+//        (F)8, (F)9, (F)0, (F)0, (F)0,
+//        (F)10, (F)0, (F)0, (F)0, (F)0;
+//    MultiDimArray<F,m> array(A);
+//    array.RST();
 
     return 0;
 }
