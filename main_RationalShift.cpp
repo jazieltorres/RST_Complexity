398ff80e87842c5a4b1410d029077821091df7ec
jazieltorres <jaziel.torres@upr.edu>
2021-04-04 14:17:42 -0400

    Runs RST_simple()

--- main_RationalShift.cpp
+++ main_RationalShift.cpp
@@ -80,8 +80,8 @@ int main(int argc, char *argv[]) {
     LegendreSeq column_seq(vertical_period);
     while (getline(cin, line)) {
         ctr++;
-        cout << "Test " << ctr << " -----------------------------" << endl;
-
+//        cout << "Test " << ctr << " -----------------------------" << endl;
+        cout << "Prime " << vertical_period << endl;
         vector<int> shift_seq(horizontal_period);
         istringstream ss(line);
         for (int i=0; i<horizontal_period; i++) {
@@ -90,15 +90,15 @@ int main(int argc, char *argv[]) {
 
         MultiDimArray<F, dim> A(shift_seq, constant, column_seq, vertical_period);
         auto start = chrono::high_resolution_clock::now();
-        A.RST();
+        A.RST_simple();
         auto stop = chrono::high_resolution_clock::now();
         auto duration = chrono::duration_cast<chrono::milliseconds>(stop - start);
         cout << "Duration:  " << duration.count() << " ms" << endl;
         cout << "Complexity:  " << A.complexity() << endl;
         cout << "Period size: " << A.period_size() << endl;
         cout << "Normalized:  " << A.normalized_complexity() << endl;
-        cout << "Groebner Basis:" << endl;
-        A.print_basis();
+//        cout << "Groebner Basis:" << endl;
+//        A.print_basis();
 //        cout << endl;
 //        A.print_array();
         cout << endl << endl;
