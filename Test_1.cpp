9e1f5127f354484bf2eef9b2c379e6f98abf1d06
jazieltorres <jaziel.torres@upr.edu>
2021-04-29 10:45:12 -0400

    TEST 1

--- Test_1.cpp
+++ Test_1.cpp
@@ -13,26 +13,28 @@ using namespace std;
 *******************************************************/
 
 int main() {
-    vector<int> seq = {1,0,0,0,1,1,1,1,0,1,0,0,0,0,1,1,0,1,1,1,1,0,1,0,0,0,1,0,1,0,0};
     const unsigned int dim = 1;
-    cout << "Length: " << seq.size() << endl;
+//    cout << "Length: " << seq.size() << endl;
     typedef NTL::GF2 F;
     F zero(0);
     F one(1);
-    blitz::TinyVector<int,1> v(seq.size());
-    MultiDimArray_GF2<dim> A(v);
-    for (int i=0; i<seq.size(); i++) {
-        blitz::TinyVector<int,1> position(i);
-        if (seq[i] == 1)
-            A.set_at(position, one);
-        else
-            A.set_at(position, zero);
-    }
+//    vector<int> seq = {1,0,0,0,1,1,1,};
+    vector<F> seq = {one, zero, zero, zero, one, one, one};
+    MultiDimArray_GF2<dim> A(seq);
+//    for (int i=0; i<seq.size(); i++) {
+//        blitz::TinyVector<int,1> position(i);
+//        if (seq[i] == 1)
+//            A.set_at(position, one);
+//        else
+//            A.set_at(position, zero);
+//    }
 
-    A.RST_simple();
 
-    cout << endl << endl;
+    A.RST_NEW();
+
+//    cout << endl << endl;
     cout << A.complexity() << endl;
+    A.print_basis();
 
     return 0;
 }
